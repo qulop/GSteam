e4de56c815159fb8f769f9973be143f0533a00b6
qulop <qlopsimail@gmail.com>
2024-05-14 21:53:16 +0300

    (Core) Added ProductRepository

---
+++ src/main/java/org/acvy/gsteam/reps/ProductRepository.java
@@ -0,0 +1,11 @@
+package org.acvy.gsteam.reps;
+
+import org.acvy.gsteam.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
