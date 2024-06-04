afffb72c01db1aa5a05bd4d263bc51d5737a076e
qulop <qlopsimail@gmail.com>
2024-06-04 17:05:43 +0300

    (Migration) Migration to Python-Flask From Java-Spring

--- src/main/java/org/acvy/gsteam/reps/DeveloperCompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package org.acvy.gsteam.reps;
-
-import org.acvy.gsteam.entities.DeveloperCompany;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeveloperCompanyRepository extends JpaRepository<DeveloperCompany, Integer> {
-
-}
