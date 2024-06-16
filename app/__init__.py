56b7a675138a3b8b6c015a9eeba03ab4bfbaa22f
qulop <qlopsimail@gmail.com>
2024-06-16 12:23:23 +0300

    (Minor) Minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,18 @@
-import flask
-import flask_restful as rest
-import flask_sqlalchemy as sql
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 
-app = flask.Flask(__name__, 
+app = Flask(__name__,
     template_folder="../templates", 
     static_folder="../static")
 app.config.from_object("config")
 
-api = rest.Api(app)
-db = sql.SQLAlchemy(app)
-
+api = Api(app)
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 migrate = Migrate(app, db, "db_repo")
 migrate.init_app(app, db)
@@ -21,9 +22,11 @@ migrate.init_app(app, db)
 from app.models import *
 from app.resources import *
 
-api.add_resource(Consumer, "/users/<int:id>")
-api.add_resource(ConsumerRegistration, "/registration/")
-api.add_resource(ItemResource, "/store/items/<int:id>")
+api.add_resource(User, "/users/<int:id>", "/users/")
+api.add_resource(UserRegistration, "/registration/")
+api.add_resource(Item, "/store/items/<int:id>", "/store/items/")
+api.add_resource(Developer, "/developers/<int:id>", "/developers/")
+api.add_resource(Cart, "/cart/<int:id>")
 
 with app.app_context():
     db.create_all()
